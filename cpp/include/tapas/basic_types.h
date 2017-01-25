46df4b85feca96bbcc01781e574913db75a5130a
Keisuke Fukuda <keisukefukuda@gmail.com>
2017-01-26 08:44:52 +0900

    removed unused functions

--- cpp/include/tapas/basic_types.h
+++ cpp/include/tapas/basic_types.h
@@ -110,31 +110,6 @@ class Region {
     os << "{" << min_ << ", " << max_ << "}";
     return os;
   }
-
-  static Region<Dim, FP> join(const Region<Dim, FP>& r) { return r; }
-  static Region<Dim, FP> join(Region<Dim, FP>&& r) { return r; }
-  
-  template <class...Args>
-  static Region<Dim,FP> join(const Region<Dim, FP>& r1, const Region<Dim, FP>& r2, Args...args) {
-    Vec<Dim, FP> cmax, cmin;
-    for (int d = 0; d < Dim; d++) {
-      cmax[d] = std::max(r1.max(d), r2.max(d));
-      cmin[d] = std::min(r1.min(d), r2.min(d));
-    }
-    return join(Region<Dim,FP>(cmin,cmax), args...);
-  }
-
-  template<class Iter>
-  static Region<Dim, FP> join(Iter beg, Iter end) {
-    Vec<Dim, FP> cmax = beg.max(), cmin = beg.min();
-    
-    for (auto iter = beg + 1; iter != end; iter++) {
-      for (int d = 0; d < Dim; d++) {
-        cmax[d] = std::max(cmax[d], iter->max(d));
-        cmin[d] = std::min(cmin[d], iter->min(d));
-      }
-    }
-  }
 };
 
 template <class TSP>
